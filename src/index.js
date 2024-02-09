434d3a71e29912211225f06b2847345d7f9f02c5
hollycosslett <154456339+hollycosslett@users.noreply.github.com>
2024-02-09 10:38:31 +0000

    Added basic JS and typewriter styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: "'The only thing we have to fear is fear itself.'",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let quoteGeneratorElement = document.querySelector("#quoteGeneratorForm");
+quoteGeneratorElement.addEventListener("submit", generateQuote);
